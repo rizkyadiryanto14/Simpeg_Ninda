cbe4aa450f8e261afb4a915ba22255e62c917379
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2022-07-03 11:11:07 +0800

    update user

--- user/dashboard.php
+++ user/dashboard.php
@@ -17,14 +17,14 @@
               $row2 = mysqli_fetch_array($selectfoto);
               if (empty($row2['foto'])) {
               ?>
-                  <img src="../build/images/user.png" class="img-circle profile_img">
+                <img src="../build/images/user.png" class="img-circle profile_img">
               <?php
-            } elseif (!empty($row2['foto'])) {
+              } elseif (!empty($row2['foto'])) {
               ?>
-                  <img src="../build/images/thump_<?php echo $row2['foto']; ?>" class="img-circle profile_img">
+                <img src="../build/images/thump_<?php echo $row2['foto']; ?>" class="img-circle profile_img">
               <?php
               }
-               ?>
+              ?>
             </li>
             <li>
               <a>
@@ -39,12 +39,12 @@
               <h3 class="name">Selamat Datang</h3>
               <?php
               include '../database/koneksi.php';
-               $select = mysqli_query($koneksi,"SELECT * FROM pegawai WHERE nip='$nip'");
+              $select = mysqli_query($koneksi, "SELECT * FROM pegawai WHERE nip='$nip'");
 
               $selectnama = mysqli_fetch_array($select);
 
               ?>
-               <h3 class="name"><?php echo $selectnama['nama_pegawai'] ?></h3>
+              <h3 class="name"><?php echo $selectnama['nama_pegawai'] ?></h3>
             </li>
           </ul>
         </div>
@@ -78,15 +78,15 @@
         <table id="datatable" class="table table-striped table-bordered table-hover">
           <thead>
             <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Jenis Cuti</th>
-                <th>Alasan Cuti</th>
-                <th>Lama Cuti</th>
-                <th>Dari Tanggal</th>
-                <th>Sampai Dengan</th>
-                <th>Status</th>
-                <th>Keterangan</th>
+              <th>No</th>
+              <th>Nama</th>
+              <th>Jenis Cuti</th>
+              <th>Alasan Cuti</th>
+              <th>Lama Cuti</th>
+              <th>Dari Tanggal</th>
+              <th>Sampai Dengan</th>
+              <th>Status</th>
+              <th>Keterangan</th>
             </tr>
           </thead>
 
@@ -94,35 +94,35 @@
 
           <tbody>
             <?php
-              include '../database/koneksi.php';
-              $nippegawai = mysqli_query($koneksi, "SELECT * FROM pegawai WHERE nip='$nip'");
-              $rowselect = mysqli_fetch_array($nippegawai);
-              $idpegawai = $rowselect['id_pegawai'];
-              $query = mysqli_query($koneksi,"SELECT * FROM cuti_pegawai cuti, pegawai pg WHERE cuti.id_pegawai = pg.id_pegawai and nip='$nip' and status_cuti='Diajukan'");
-              $i = 1;
-              while ($row = mysqli_fetch_array($query)) {
-             ?>
-             <tr>
-               <td><?php echo $i ?></td>
-               <td><?php echo $row['nama_pegawai'] ?></td>
-               <td><?php echo $row['jenis_cuti'] ?></td>
-               <td><?php echo $row['alasan_cuti'] ?></td>
-               <td><?php echo $row['lama_cuti'];?> <?php echo $row['ket_lama_cuti'];  ?></td>
-               <td><?php echo $row['dari_tanggal']; ?></td>
-               <td><?php echo $row['sampai_dengan']; ?></td>
-               <td class="text-center">
-                 <a href="#" class="btn btn-success btn-xs "> <?php echo $row['status_cuti']; ?></a>
-               </td>
-               <td class="text-center">
-                 <a href="#" class="btn btn-primary btn-xs "> <?php echo $row['ket_status_cuti']; ?></a>
-               </td>
-             </tr>
+            include '../database/koneksi.php';
+            $nippegawai = mysqli_query($koneksi, "SELECT * FROM pegawai WHERE nip='$nip'");
+            $rowselect = mysqli_fetch_array($nippegawai);
+            $idpegawai = $rowselect['id_pegawai'];
+            $query = mysqli_query($koneksi, "SELECT * FROM cuti_pegawai cuti, pegawai pg WHERE cuti.id_pegawai = pg.id_pegawai and nip='$nip' and status_cuti='Diajukan'");
+            $i = 1;
+            while ($row = mysqli_fetch_array($query)) {
+            ?>
+              <tr>
+                <td><?php echo $i ?></td>
+                <td><?php echo $row['nama_pegawai'] ?></td>
+                <td><?php echo $row['jenis_cuti'] ?></td>
+                <td><?php echo $row['alasan_cuti'] ?></td>
+                <td><?php echo $row['lama_cuti']; ?> <?php echo $row['ket_lama_cuti'];  ?></td>
+                <td><?php echo $row['dari_tanggal']; ?></td>
+                <td><?php echo $row['sampai_dengan']; ?></td>
+                <td class="text-center">
+                  <a href="#" class="btn btn-success btn-xs "> <?php echo $row['status_cuti']; ?></a>
+                </td>
+                <td class="text-center">
+                  <a href="#" class="btn btn-primary btn-xs "> <?php echo $row['ket_status_cuti']; ?></a>
+                </td>
+              </tr>
 
 
-             <?php
-             $i++;
-           }
-              ?>  
+            <?php
+              $i++;
+            }
+            ?>
           </tbody>
         </table>
       </div>
@@ -130,14 +130,14 @@
   </div>
 </div>
 <div class="row">
-<div class="ccol-md-12 col-sm-12 col-xs-12">
-  <div class="x_panel">
-    <div class="x_content text-center">
-      <h1><i class="fa fa-university"></i> DPPA</h1>
-      <p>Data Pegawai Pengadilan Agama</p>
-      <p></p>
-      <p>©2020 Pengadilan Agama Karawang, All Rights Reserved.</p>
+  <div class="ccol-md-12 col-sm-12 col-xs-12">
+    <div class="x_panel">
+      <div class="x_content text-center">
+        <h1><i class="fa fa-university"></i> SIMPEG</h1>
+        <p>Data Pegawai SDIT Insan Qur'ani Sumbawa</p>
+        <p></p>
+        <p>© <?= date('Y'); ?> SDIT Insan QUr'ani Sumbawa</p>
+      </div>
     </div>
   </div>
-</div>
 </div>
